b60cdb6c90a4e1a133a6d13e94cda8b88238ca56
Brian Leathem <bleathem@gmail.com>
2012-11-27 20:36:59 -0800

    Added a IE safe wrapper around console.log

--- src/main/webapp/html5-bootstrap.js
+++ src/main/webapp/html5-bootstrap.js
@@ -10,22 +10,22 @@ $(function () {
             dataType:'json',
             type:'GET',
             success:function (capitals) {
-                console.log("success, capitals: " + capitals);
+                log("success, capitals: " + capitals);
                 $.ajax('rs/capitals/selected', {
                     dataType:'json',
                     type:'GET',
                     success:function (selectedCapitals) {
-                        console.log("success, selectedCapitals: " + selectedCapitals);
+                        log("success, selectedCapitals: " + selectedCapitals);
                         initPickList();
                         $('#myPickList').pickListGlue("populatePickList", capitals, selectedCapitals);
                         subscribe();
                     }
                 }).error(function () {
-                        console.log("error retrieving all capitals");
+                        log("error retrieving all capitals");
                     });
             }
         }).error(function () {
-                console.log("error retrieving all capitals");
+                log("error retrieving all capitals");
             });
     }
 
@@ -43,12 +43,12 @@ $(function () {
             type:'POST',
             data:JSON.stringify(selectedCapitals),
             success:function (data) {
-                console.log("Ajax capital post success");
+                log("Ajax capital post success");
             }
         }).error(function (error) {
                 var errorMessage = $.parseJSON(error.responseText);
                 $.each(errorMessage, function (index, message) {
-                    console.log("Ajax capital post error: " + message);
+                    log("Ajax capital post error: " + message);
                 });
             });
     }
@@ -59,18 +59,18 @@ $(function () {
             logLevel:'debug',
             transport:'long-polling',
             onMessage:function (response) {
-                console.log(response.responseBody);
+                log(response.responseBody);
                 var selectedCapitals = JSON.parse(response.responseBody);
                 $('#myPickList').pickListGlue("updatePickList", selectedCapitals);
             },
             onOpen:function (response) {
-                console.log('Atmosphere connected using ' + response.transport);
+                log('Atmosphere connected using ' + response.transport);
             },
             onReconnect:function (request, response) {
-                console.log("reconnecting");
+                log("reconnecting");
             },
             onError: function(response) {
-                console.log("socket or server problem")
+                log("socket or server problem")
             }
         };
 
